c332280682929ea30085f554f0d1eb5614a9ec26
TranPhuongDinh-19120476-HCMUS <19120476@student.hcmus.edu.vn>
2023-06-04 16:01:36 +0000

    Not use proxy

--- bigbluebutton-html5/imports/api/presentations/server/methods/insertDocumentWithURL.js
+++ bigbluebutton-html5/imports/api/presentations/server/methods/insertDocumentWithURL.js
@@ -1,21 +1,39 @@
-import axios from "axios";
-import { Meteor } from "meteor/meteor";
+import sha1 from 'js-sha1';
+import axios from 'axios';
+import { Meteor } from 'meteor/meteor';
+import convert from 'xml-js';
 
-const PROXY_HOST = Meteor.settings.private.app.proxyHost || "";
+const BBB_SECRET = Meteor.settings.private.app.bbbSecret || '';
+const SERVER_HOST = Meteor.settings.private.app.serverHost || '';
+
+const createChecksum = (apiCall, params) => {
+  const queryString = new URLSearchParams(params).toString();
+  return sha1(`${apiCall}${queryString}${BBB_SECRET}`);
+};
+
+const resToObject = (res) => {
+  const rawObject = convert.xml2js(res.data, { compact: true, spaces: 4 });
+
+  const finalObject = {};
+
+  Object.keys(rawObject.response).forEach((key) => (finalObject[key] = rawObject.response[key]._text));
+
+  return finalObject;
+};
 
 export default async function insertDocumentWithURL({ files, meetingID }) {
   const params = {
     meetingID,
-    apiCall: "insertDocument",
+    checksum: createChecksum('insertDocument', { meetingID }),
   };
 
-  const res = await axios.post(
-    `${PROXY_HOST}/bigbluebutton/api`,
-    { files: JSON.stringify(files) },
-    {
-      params,
-    },
-  );
+  const filesXML = files.map((f) => `<module name="presentation"><document url="${f.url}" filename="${f.name}" downloadable="true" /></module>`).join('');
 
-  return res.data;
+  const res = await axios.post(`${SERVER_HOST}/bigbluebutton/api/insertDocument?${new URLSearchParams(params)}`, `<?xml version="1.0" encoding="UTF-8"?><modules>${filesXML}</modules>`, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+
+  return resToObject(res);
 }
